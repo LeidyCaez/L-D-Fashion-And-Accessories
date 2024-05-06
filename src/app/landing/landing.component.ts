ea89ae26f95f82b66f9895238b34b5219d8ca6ff
carlos.iriarte <carlos.iriarte@amerikatis.com>
2024-05-06 08:54:08 -0500

    Mejoras

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-landing',
@@ -8,6 +9,19 @@ import { LayoutService } from 'src/app/layout/service/app.layout.service';
 })
 export class LandingComponent {
 
-    constructor(public layoutService: LayoutService, public router: Router) { }
+    constructor(public layoutService: LayoutService, public router: Router, private _sanitizer: DomSanitizer) { }
+
+    getVideoIframe(url) {
+        var video, results;
+     
+        if (url === null) {
+            return '';
+        }
+        results = url.match('[\\?&]v=([^&#]*)');
+        video   = (results === null) ? url : results[1];
+     
+        return this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + video);   
+    }
+    
     
 }
